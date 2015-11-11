c87f2db390f8371e0a28a2e87fd735d4cf1bc3f9
Flaverton Rodrigues Rosa <flavertonrodrigues@gmail.com>
2015-11-11 15:40:24 -0200

    No long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 import typoshot
-import os
-
-
-def read(file_name):
-    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
 setup(
@@ -12,14 +7,12 @@ setup(
     version=typoshot.__version__,
     author="Flaverton Rodrigues Rosa",
     author_email="contato@flaverton.com",
-    description=("Typo is a script to take screenshot on your website/blog."
-                 " A simpler way to check printing, viewing the page in complete."),
+    description=("Check your website/blog typography simple, with screenshots!"),
     license="MIT",
     keywords="splinter screenshot typo typography",
     url="https://github.com/flavertonrr/typo-shot",
     scripts=["typo"],
     packages=["typoshot"],
-    long_description=read('README'),
     install_requires=[
         "splinter>=0.7.3",
         "requests>=2.4.3"
